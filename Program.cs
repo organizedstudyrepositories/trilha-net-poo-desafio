98deb969160892e4afafb0bc9c960bfc039f3e07
organizedstudyrepositories <eliseucontareserva@gmail.com>
2024-01-04 11:07:21 -0300

    Adiciona features para completar exercício e atualiza versão do .NET no csproj para 8.0

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123456", "modelo 01", "111111", 64);
+nokia.Ligar();
+Console.WriteLine("Celular Nokia ligado");
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Candy Crush");
+
+Console.WriteLine("\n");
+Console.WriteLine("###########");
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone(numero: "654321", modelo: "modelo 02", imei: "222222", memoria: 128);
+iphone.Ligar();
+Console.WriteLine("Celular Iphone ligado");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Sudoku");
